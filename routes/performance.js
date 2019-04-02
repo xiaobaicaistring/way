ce310acf42fccf9e37fcb882a98629d82b90fb84
zhaiyafei <zhaiyafei@huami.com>
2019-04-02 18:37:48 +0800

    fix(core): fix params

--- routes/performance.js
+++ routes/performance.js
@@ -13,8 +13,8 @@ client.on("error", function (err) {
 // etp: inn
 // ep: {"startTime":1554199581965,"lookupDomainTime":0,"connectTime":25,"requestTime":152,"networkTime":183,"domReadyTime":1088,"firstPaintTime":188}
 router.get('/', function(req, res, next) {
-  console.log(req.query.ep)
-  var data = req.query.ep
+  console.log(JSON.stringify(req.query.ep))
+  var data = JSON.stringify(req.query.ep)
   client.lpush('logstash', data)
   res.send('success');
 });
