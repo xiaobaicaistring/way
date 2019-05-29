9b9165c57754f28468b6b114a2196cccebb44652
zhaiyafei <zhaiyafei@huami.com>
2019-05-29 17:31:00 +0800

    feat(core): test

--- routes/performance.js
+++ routes/performance.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var redis = require("redis");
-var client = redis.createClient();
+var kafka = require('kafka-node');
+var Producer = kafka.Producer;
+var client = new kafka.KafkaClient({kafkaHost: 'localhost:9092'});
+var producer = new Producer(client);
 
 client.on("error", function (err) {
   console.log("Error " + err);
@@ -13,7 +15,15 @@ client.on("error", function (err) {
 // etp: inn
 // ep: {"startTime":1554199581965,"lookupDomainTime":0,"connectTime":25,"requestTime":152,"networkTime":183,"domReadyTime":1088,"firstPaintTime":188}
 router.get('/', function(req, res, next) {
-  client.lpush('logstash', dreq.query.ep)
+  // client.lpush('logstash', dreq.query.ep)
+  var payloads = [
+    { topic: 'topic1', messages: 'hi', partition: 0 },
+    { topic: 'topic2', messages: ['hello', 'world', km] }
+  ];
+  producer.send(payloads, function (err, data) {
+    console.log(err)
+    console.log(data);
+  });
   res.send('success');
 });
 
