ec3886649d30af7e485a5782bc02f2676f7acf82
zhaiyafei <zhaiyafei@huami.com>
2019-05-29 18:43:19 +0800

    f

--- routes/performance.js
+++ routes/performance.js
@@ -10,20 +10,20 @@ var producer = new Producer(client);
 // et: 1554199583097
 // etp: inn
 // ep: {"startTime":1554199581965,"lookupDomainTime":0,"connectTime":25,"requestTime":152,"networkTime":183,"domReadyTime":1088,"firstPaintTime":188}
-router.get('/', function(req, res, next) {
-  // client.lpush('logstash', dreq.query.ep)
+producer.on('ready', async function () {
+  router.get('/', function(req, res, next) {
     var payloads = [
       { topic: 'test', messages: 'hi'},
       { topic: 'test', messages: ['hello', 'world', '666'] }
     ];
-    producer.on('ready', async function () {
-      producer.send(payloads, function (err, data) {
-        console.log(err)
-        console.log(data);
-      });  
-  })
+    
+    producer.send(payloads, function (err, data) {
+      console.log(err)
+      console.log(data);
+    }); 
 
-  res.send('success');
-});
+    res.send('success');
+  }); 
+})
 
 module.exports = router;
