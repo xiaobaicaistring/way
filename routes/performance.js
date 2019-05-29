1dae7035cd6cb6238d7124ed9d824734ba2fa2fc
zhaiyafei <zhaiyafei@huami.com>
2019-05-29 18:36:29 +0800

    xx

--- routes/performance.js
+++ routes/performance.js
@@ -2,12 +2,8 @@ var express = require('express');
 var router = express.Router();
 var kafka = require('kafka-node');
 var Producer = kafka.Producer;
-var client = new kafka.Client('localhost:9092');
+var client = new kafka.Client('10.1.15.64:2181');
 var producer = new Producer(client);
-
-client.on("error", function (err) {
-  console.log("Error " + err);
-});
 
 /* GET home page. */
 // ei: front_performance_base
@@ -16,14 +12,17 @@ client.on("error", function (err) {
 // ep: {"startTime":1554199581965,"lookupDomainTime":0,"connectTime":25,"requestTime":152,"networkTime":183,"domReadyTime":1088,"firstPaintTime":188}
 router.get('/', function(req, res, next) {
   // client.lpush('logstash', dreq.query.ep)
-  var payloads = [
-    { topic: 'topic1', messages: 'hi', partition: 0 },
-    { topic: 'topic2', messages: ['hello', 'world', '666'] }
-  ];
-  producer.send(payloads, function (err, data) {
-    console.log(err)
-    console.log(data);
-  });
+    var payloads = [
+      { topic: 'test', messages: 'hi'},
+      { topic: 'test', messages: ['hello', 'world', '666'] }
+    ];
+    producer.on('ready', async function () {
+      producer.send(payloads, function (err, data) {
+        console.log(err)
+        console.log(data);
+      });  
+  })
+
   res.send('success');
 });
 
